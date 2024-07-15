b48474c50c3063ad2105273cf5f518ab17796694
JohnN320 <hashir.waqass@gmail.com>
2024-07-15 11:58:28 +0500

    first version

--- app/src/main/java/com/example/fleeto/AdminDash.java
+++ app/src/main/java/com/example/fleeto/AdminDash.java
@@ -2,15 +2,19 @@ package com.example.fleeto;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +23,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -27,6 +32,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,6 +44,9 @@ public class AdminDash extends AppCompatActivity {
     DatabaseReference Ref;
     ProgressDialog progressDialog;
 
+    BottomNavigationView navigationView;
+    ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +57,44 @@ public class AdminDash extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+
+        actionBar = getSupportActionBar(); // Initialize the actionBar correctly
+        if (actionBar != null) {
+            actionBar.setTitle("Profile");
+        }
+
+        UserProfileFragment fragment1 = new UserProfileFragment();
+        FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+        ft1.replace(R.id.content, fragment1, "");
+        ft1.commit();
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener selectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if(item.getItemId() == R.id.nav_profile) {
+                        if (actionBar != null) {
+                            actionBar.setTitle("Profile");
+                        }
+                        UserProfileFragment fragment1 = new UserProfileFragment();
+                        FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+                        ft1.replace(R.id.content, fragment1, "");
+                        ft1.commit();
+                        return true;
+                    }
+
+                    else {
+                        return false;
+                    }
+                }
+            };
+
 }
 //        mAuth = FirebaseAuth.getInstance();
 //
